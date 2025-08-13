0a76fadb43b6f4f3340e4f15772b5f5925e15b62
Francue Werner <90242795+FirdavsSalokhiddinov@users.noreply.github.com>
2025-08-14 04:56:49 +0900

    deleted

--- frontend/src/api/supabase-client.js
+++
@@ -1,8 +0,0 @@
-import { createClient } from '@supabase/supabase-js'
-
-// const supabaseUrl = import.meta.env.KEASY_SUPABASE_URL
-// const supabaseAnonKey = import.meta.env.KEASY_SUPABASE_ANON_KEY
-const supabaseUrl = "https://lnbvhkwfhvkinziybkbo.supabase.co"
-const supabaseAnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImxuYnZoa3dmaHZraW56aXlia2JvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTUwOTkyMTksImV4cCI6MjA3MDY3NTIxOX0.yNzOb9ozrc5cKPXvExqgfVPm0hQBvorQaQmMy3dLClw"
-
-export const supabase = createClient(supabaseUrl, supabaseAnonKey)
