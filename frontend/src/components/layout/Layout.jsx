e13d9419c53c1f68a4dae3bdc39e8ea5db4c951c
FirdavsSalokhiddinov <90242795+FirdavsSalokhiddinov@users.noreply.github.com>
2025-09-13 08:01:55 +0900

    boom

--- frontend/src/components/layout/Layout.jsx
+++ frontend/src/components/layout/Layout.jsx
@@ -7,7 +7,7 @@ const Layout = ({ children}) => {
   const location = useLocation();
 
   // Define routes where the feature is upcoming
-  const upcomingRoutes = ['/blog', '/nearby', '/community', '/events', `/profile/id`, '"/edit-profile/', '/marketplace', '/marketplace/post', '/marketplace/my', `/marketplace/id`, `/marketplace/edit/id`, '/settings', '/about', '/contact', '/faq', ]; // add routes you want to block
+  const upcomingRoutes = ['/blog', '/nearby', '/community', '/events', `/profile/id`, '"/edit-profile/', '/marketplace', '/marketplace/post', '/marketplace/my', `/marketplace/id`, `/marketplace/edit/id`, '/settings', '/about', '/contact', '/faq', '/blog/', '/nearby/', '/community/', '/events/'];
   const showUpcoming = upcomingRoutes.includes(location.pathname);
 
   return (
